052280689974a9fa51718fda197a3d1269dba4f8
Gili <gilad@msbitsoftware.com>
2018-10-21 22:56:07 +0300

    git remote add origin https://github.com/giladym/angular-material-autocomplease.git git push -u origin master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+
+export interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'autocompleate-material';
+
+  myControl = new FormControl();
+  myControl2 = new FormControl();
+  options: User[] = [
+    {name: 'Mary'},
+    {name: 'Shelley'},
+    {name: 'Igor'},
+    {name: 'Debby'},
+    {name: 'Madona'}
+  ];
+  filteredOptions: Observable<User[]>;
+  filteredOptions2: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith<string | User>(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter2(name) : this.options.slice())
+      );
+
+      this.filteredOptions2 = this.myControl2.valueChanges
+      .pipe(
+        startWith<string | User>(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter2(name) : this.options.slice())
+      );
+  }
+
+  displayFn(user?: User): string | undefined {
+    return user ? user.name : undefined;
+  }
+
+  // Display value autocomplete
+  private _filter1(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  // Filter autocomplete
+  private _filter2(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
 }
